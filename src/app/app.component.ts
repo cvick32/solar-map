d0f6f62e5735564178d03ba8cfc0896a12f1ae23
cole <cole.d.vick@gmail.com>
2020-02-05 10:58:06 -0500

    add inital map setup per https://medium.com/@jkeung/integrating-google-maps-api-w-angular-7-e7672396ce2d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'solar-map';
+  @ViewChild('map') mapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties)
+  }
+
 }
