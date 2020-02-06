8339e664b528d6c3db380d91e065c7e616f8d2b3
cole <cole.d.vick@gmail.com>
2020-02-05 21:03:28 -0500

    small model for a Location

---
+++ src/app/location.model.ts
@@ -0,0 +1,9 @@
+/**
+ * Type for a Location. This is the exact form
+ * that the location data comes in from the
+ * geocode api.
+ */
+export interface Location {
+  lat: number;
+  lng: number;
+}
