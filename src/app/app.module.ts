c8f76f30253871bb075aaebe4203e722c089248b
cole <cole.d.vick@gmail.com>
2020-02-05 21:02:57 -0500

    add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AngularMaterialModule } from './angular-material.module';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+import { MapComponent } from './map/map.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AngularMaterialModule
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
